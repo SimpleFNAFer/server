d373c4451c3eb4b29280f9e20b7dbab0d7eebe3b
Simple FNAFer <tncheetah758@gmail.com>
2025-01-13 05:39:54 +0300

    corrected js

--- template/admin-login.js
+++ template/admin-login.js
@@ -1,31 +1,60 @@
-document.addEventListener("DOMContentLoaded", function () {
+function handleLoginButtonClick(event) {
+    const container = event.target.parentNode;
+    const login = container.querySelector('#login');
+    const password = container.querySelector('#password');
 
-    function handleLoginButtonClick(event) {
-        const container = event.target.parentNode;
-        const login = container.querySelector('#login');
-        const password = container.querySelector('#password');
+    if (login && password) {
+        const loginValue = login.value;
+        const passwordValue = password.value;
 
-        if (login && password) {
-            const loginValue = login.value;
-            const passwordValue = password.value;
+        if (loginValue !== "" && passwordValue !== "") {
+            const formData = new FormData();
+            formData.append("login", loginValue);
+            formData.append("password", passwordValue);
 
-            if (loginValue !== "" && passwordValue !== "") {
-                const formData = new FormData();
-                formData.append("login", loginValue);
-                formData.append("password", passwordValue);
-
-                fetch("/admin-login", {
-                    method: "POST",
-                    body: formData,
-                    credentials: "include",
-                    redirect: "follow"
+            fetch("/admin-login", {
+                method: "POST",
+                body: formData,
+                credentials: "include",
+                redirect: "follow"
+            })
+                .then(response => {
+                        if (response.redirected) {
+                            window.location.replace(response.url);
+                        }
+                        else {
+                            return response.text()
+                        }
+                    }
+                )
+                .then((html) => {
+                    if (html) {
+                        const newUrl = window.location.href;
+                        history.pushState({ html: html }, '', newUrl);
+                        document.body.innerHTML = html;
+                    }
                 })
-            }
         }
     }
+}
 
+function attachEventListeners() {
     const loginButton = document.querySelectorAll('.input_button');
     loginButton.forEach(button => {
         button.addEventListener("click", handleLoginButtonClick);
     });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    history.replaceState({ html: document.body.innerHTML }, '', window.location.href);
+
+    attachEventListeners();
 });
+
+window.onpopstate = function(event) {
+    if (event.state && event.state.html) {
+        document.body.innerHTML = event.state.html;
+    }
+
+    attachEventListeners()
+};
--- template/admin.js
+++ template/admin.js
@@ -1,47 +1,78 @@
-document.addEventListener("DOMContentLoaded", function () {
+function handleFrequentButtonClick(event) {
+    const container = event.target.parentNode;
+    const frequentIpInput = container.querySelector('.frequent_ip');
 
-    function handleFrequentButtonClick(event) {
-        const container = event.target.parentNode;
-        const frequentIpInput = container.querySelector('.frequent_ip');
+    if (frequentIpInput) {
+        const ipValue = frequentIpInput.textContent;
 
-        if (frequentIpInput) {
-            const ipValue = frequentIpInput.textContent;
+        if (ipValue !== "") {
+            const formData = new FormData();
+            formData.append("ip", ipValue);
 
-            if (ipValue !== "") {
-                const formData = new FormData();
-                formData.append("ip", ipValue);
-
-                fetch("/block", {
-                    method: "POST",
-                    body: formData,
-                    credentials: "include",
-                    redirect: "follow"
+            fetch("/block", {
+                method: "POST",
+                body: formData,
+                credentials: "include",
+                redirect: "follow"
+            })
+                .then(response => {
+                        if (response.redirected) {
+                            window.location.replace(response.url);
+                        }
+                        else {
+                            return response.text()
+                        }
+                    }
+                )
+                .then((html) => {
+                    if (html) {
+                        const newUrl = window.location.href;
+                        history.pushState({ html: html }, '', newUrl);
+                        document.body.innerHTML = html;
+                    }
                 })
-            }
         }
     }
+}
 
-    function handleBlockedButtonClick(event) {
-        const container = event.target.parentNode;
-        const blockedIpInput = container.querySelector('.blocked_ip');
+function handleBlockedButtonClick(event) {
+    const container = event.target.parentNode;
+    const blockedIpInput = container.querySelector('.blocked_ip');
 
-        if (blockedIpInput) {
-            const ipValue = blockedIpInput.textContent;
+    if (blockedIpInput) {
+        const ipValue = blockedIpInput.textContent;
 
-            if (ipValue !== "") {
-                const formData = new FormData();
-                formData.append("ip", ipValue);
+        if (ipValue !== "") {
+            const formData = new FormData();
+            formData.append("ip", ipValue);
 
-                fetch("/unblock", {
-                    method: "POST",
-                    body: formData,
-                    credentials: "include",
-                    redirect: "follow"
+            fetch("/unblock", {
+                method: "POST",
+                body: formData,
+                credentials: "include",
+                redirect: "follow"
+            })
+                .then(response => {
+                        if (response.redirected) {
+                            window.location.replace(response.url);
+                        }
+                        else {
+                            return response.text()
+                        }
+                    }
+                )
+                .then((html) => {
+                    if (html) {
+                        const newUrl = window.location.href;
+                        history.pushState({ html: html }, '', newUrl);
+                        document.body.innerHTML = html;
+                    }
                 })
-            }
         }
     }
+}
 
+function attachEventListeners() {
     const frequentButtons = document.querySelectorAll('.frequent_button');
     frequentButtons.forEach(button => {
         button.addEventListener("click", handleFrequentButtonClick);
@@ -51,4 +82,18 @@ document.addEventListener("DOMContentLoaded", function () {
     blockedButtons.forEach(button => {
         button.addEventListener("click", handleBlockedButtonClick);
     });
+}
+
+window.onpopstate = function(event) {
+    if (event.state && event.state.html) {
+        document.body.innerHTML = event.state.html;
+    }
+
+    attachEventListeners()
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    history.replaceState({ html: document.body.innerHTML }, '', window.location.href);
+
+    attachEventListeners();
 });
